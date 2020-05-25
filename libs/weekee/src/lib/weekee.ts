4fe219abe42819e32ef6d6a57b2e9ebd0a03f405
Trevor Sargent <trevor@lucid.rocks>
2020-05-24 19:20:19 -0700

    chore(weekee): scaffold initDb

--- libs/weekee/src/lib/weekee.ts
+++ libs/weekee/src/lib/weekee.ts
@@ -1,1 +1,12 @@
-export const initDb = () => {}
+export const initDb = async () => {
+  console.log(`Filling Database with Wikipedia's dirty dirty garbage...`)
+
+  const done = await new Promise<boolean>(() => {
+    setTimeout((): boolean => {
+      console.log('... and Done!')
+      return true
+    }, 1000)
+  })
+
+  return done
+}
