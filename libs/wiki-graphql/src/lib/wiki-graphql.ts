8e57bd5738288054ba293849e878c36b4def1a3c
Trevor Sargent <trevor@lucid.rocks>
2020-05-27 22:39:44 -0700

    feat(wiki-graphql): add links to graph

--- libs/wiki-graphql/src/lib/wiki-graphql.ts
+++ libs/wiki-graphql/src/lib/wiki-graphql.ts
@@ -9,6 +9,7 @@ import {
   ResolverInterface,
   FieldResolver,
   Root,
+  Int,
 } from 'type-graphql'
 import { articles, article, Article as A, ID, ArticleType } from '@labs/weekee'
 import { ApolloServer } from 'apollo-server'
@@ -18,7 +19,7 @@ export class Article implements A {
   @Field((type) => String)
   type: ArticleType
 
-  @Field()
+  @Field((type) => String)
   id: string
 
   @Field()
@@ -29,18 +30,30 @@ export class Article implements A {
 
 @Resolver((of) => Article)
 export class ArticleResolver {
+  @Query((type) => Int)
+  async numArticles(): Promise<number> {
+    return (await articles()).length
+  }
+
   @Query((returns) => [Article])
   async articles() {
     return await articles()
   }
 
-  @FieldResolver((returns) => Article)
-  async linksTo(@Root('article') a: Article) {
-    return
+  @FieldResolver((returns) => Int)
+  async numLinks(@Root() a: Article) {
+    return a.linksTo.length
+  }
+
+  @FieldResolver((returns) => [Article])
+  async linksTo(@Root() a: Article) {
+    const links = await articles(a.linksTo)
+    return links
   }
 }
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4101
+console.log(PORT)
 
 export async function startWikiGraphQl() {
   // ... Building schema here
@@ -63,5 +76,3 @@ export async function startWikiGraphQl() {
     console.error(e)
   }
 }
-
-startWikiGraphQl()
