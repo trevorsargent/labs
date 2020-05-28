76b98d09b5517571eb75323e8228e825f6a1dfd8
Trevor Sargent <trevor@lucid.rocks>
2020-05-28 00:22:09 -0700

    build(graph): add generated graph and graph service

---
+++ apps/wiki-bacon/src/app/services/graph.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core'
+import { Apollo } from 'apollo-angular'
+import ApolloClient from 'apollo-client'
+
+import createMayEppGraph, { Client } from '@labs/generated/may-epp.client'
+
+let proxy: Client | null = null
+
+export const getProxy = () => proxy
+
+@Injectable()
+export class GraphService {
+  public client: ApolloClient<any>
+  private proxy: Client
+
+  constructor(public apollo: Apollo) {
+    this.client = apollo.getClient()
+
+    this.proxy = createMayEppGraph(this.client, {
+      watchQuery: { fetchPolicy: 'cache-and-network' as any },
+    })
+
+    proxy = this.proxy
+  }
+
+  get query() {
+    return this.proxy.query
+  }
+
+  get refetchQuery() {
+    return this.proxy.refetchQuery
+  }
+
+  get mutation() {
+    return this.proxy.mutation
+  }
+
+  get watchQuery() {
+    return this.proxy.watchQuery
+  }
+
+  deleteNode(id: any) {
+    this.client.cache['data'].delete(id)
+  }
+}
