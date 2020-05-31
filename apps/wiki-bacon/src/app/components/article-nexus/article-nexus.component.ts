b21c368ba4059caee1fd4c1fd87d347cb485cdce
Trevor Sargent <trevor@lucid.rocks>
2020-05-31 16:11:20 -0700

    wip(wiki-bacon): begin sketch of article nexus

--- apps/wiki-bacon/src/app/components/article-nexus/article-nexus.component.ts
+++ apps/wiki-bacon/src/app/components/article-nexus/article-nexus.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { GraphService } from '../../services/graph.service'
+import { ComponentBase } from '../component'
+import { Observable } from 'rxjs'
+import { Article } from 'generated/may-epp.client'
+import { NEXUS_ARTICLE_FRAGMENT } from './article-nexus.graphql'
 
 @Component({
   selector: 'wiki-article-nexus',
   templateUrl: './article-nexus.component.html',
-  styleUrls: ['./article-nexus.component.scss']
+  styleUrls: ['./article-nexus.component.scss'],
 })
-export class ArticleNexusComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ArticleNexusComponent extends ComponentBase
+  implements OnInit, OnDestroy {
+  constructor(private graph: GraphService) {
+    super()
   }
 
+  articles$: Observable<Article[]>
+
+  ngOnInit(): void {
+    super.ngOnInit()
+
+    // SET UP (RX)
+
+    this.articles$ = this.graph.watchQuery.articles(NEXUS_ARTICLE_FRAGMENT)
+  }
+
+  ngOnDestroy() {
+    super.ngOnDestroy()
+  }
 }
--- apps/wiki-bacon/src/app/components/article-node/article-node.component.ts
+++ apps/wiki-bacon/src/app/components/article-node/article-node.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core'
+import { Article } from 'generated/may-epp.client'
 
 @Component({
   selector: 'wiki-article-node',
   templateUrl: './article-node.component.html',
-  styleUrls: ['./article-node.component.scss']
+  styleUrls: ['./article-node.component.scss'],
 })
 export class ArticleNodeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input()
+  article: Article
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
